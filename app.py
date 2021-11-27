f5c4a2bb79abf2e604d1b93e1b6a1fce5f347b34
kama999 <ka-super-kamil@yandex.ru>
2021-11-27 18:27:29 +0300

    k1
    Созданы файлы для разных тестов

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -7,20 +7,20 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@app.route('/developer/')
+@app.route('/developer/', methods=['POST','GET'])
 def developer():
     print ('I got clicked1!')
-    return render_template('index.html')
+    return render_template('developer.html')
 
-@app.route('/tester/')
+@app.route('/tester/', methods=['POST','GET'])
 def tester():
     print ('I got clicked2!')
-    return render_template('index.html')
+    return render_template('tester.html')
 
-@app.route('/analite/')
+@app.route('/analitix/', methods=['POST','GET'])
 def analite():
     print ('I got clicked3!')
-    return render_template('index.html')
+    return render_template('analitix.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
