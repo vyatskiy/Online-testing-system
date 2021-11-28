2fb61abe2637312a2b2657b91c6a6284c79092be
kama999 <ka-super-kamil@yandex.ru>
2021-11-28 20:53:26 +0300

    k1
    Добавлены формы для вопросов

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 from flask import Flask, render_template, redirect
+from questions import QAquestions, AnalitixQuestions, DevelopersQuestions
 
 app = Flask(__name__)
 
-
+questions = [0]*10
 @app.route('/')
 def hello():
     return render_template('index.html')
 
 @app.route('/developer/', methods=['POST','GET'])
 def developer():
-    print ('I got clicked1!')
-    return render_template('developer.html')
+    questions[0] = DevelopersQuestions.D1
+    questions[1] = DevelopersQuestions.D2
+    return render_template('developer.html', questions = questions)
 
 @app.route('/tester/', methods=['POST','GET'])
 def tester():
-    print ('I got clicked2!')
-    return render_template('tester.html')
+    questions[0] = QAquestions.QA1
+    questions[1] = QAquestions.QA2
+    return render_template('tester.html', questions = questions)
 
 @app.route('/analitix/', methods=['POST','GET'])
 def analite():
-    print ('I got clicked3!')
-    return render_template('analitix.html')
+    questions[0] = AnalitixQuestions.A1
+    questions[1] = AnalitixQuestions.A2
+    return render_template('analitix.html', questions = questions)
+
+@app.route('/save/', methods=['POST','GET'])
+def save():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
