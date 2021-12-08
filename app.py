6e532d646e0013d1928ce30793a8c3b5e868a4e3
kama999 <ka-super-kamil@yandex.ru>
2021-12-08 17:32:30 +0300

    main
    Исправлены недочеты

--- app.py
+++ app.py
@@ -294,11 +294,11 @@ def save_answers():
                 insert_data(True, Answers.name, Answers.surname, Answers.city, Answers.age)
                 insert_answers_bd(data, FIRST, SECOND, THIRD,
                 FOURTH, FIFTH, sixth_answer, seventh_answer, eighth_answer)
-                TYPE_TEST.OPEN_FORM == 1
+                TYPE_TEST.OPEN_FORM == 0
                 TYPE_TEST.type_test = 'None'
                 return render_template('correct_answers.html', correct = CORRECTS)
 
-    flash("Ошибка: попытка повторного прохождения теста!", category='error')    
+    flash("Ошибка сервера! Попробуйте обновить страницу", category='error')    
     return render_template('index.html')            
 
 def insert_answers_bd(data, FIRST, SECOND, THIRD, FOURTH, FIFTH, sixth_answer, seventh_answer, eighth_answer):
